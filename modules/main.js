9b394166dec4062fb10602d5119397fbbf8eb2b5
Vladimir Aseev <nemesisv@mail.ru>
2020-05-06 00:10:45 +0300

    form impulse to degree change support from esp32 to esp8266

--- modules/main.js
+++ modules/main.js
@@ -48,23 +48,154 @@ function initForwardBackWard(main) {
 function initKeep(main) {
     const rotateBox = main.querySelector('#RotateBox');
     rotateBox.addEventListener('mousedown', async function (event) {
+        if (event.target != this) return;
         gEngines.hold = roboarm.hold;
         await roboarm.set(gEngines);
     });
     rotateBox.addEventListener('mouseup', async function (event) {
+        if (event.target != this) return;
         gEngines.hold = roboarm.release;
         await roboarm.set(gEngines);
     });
 }
 
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            return resolve()
+        }, ms)
+    });
+}
 
+function deadZone(val) {
+    const threshold = 10;
+    const hLimit = 180 - threshold;
+    if (val > hLimit) return hLimit;
+    if (val < threshold) return threshold;
+    return val;
+}
+
+function alleviate(sStart, sEnd, dStart, dEnd) {
+    const delta = Math.abs(dEnd - dStart);
+    const mPath = Math.trunc((sEnd - sStart) / 2);
+    const maxDelta = 60
+    if (delta < maxDelta) return deadZone(sStart);
+    const finish = sStart + mPath;
+    return deadZone(finish);
+}
+
+
+async function command({ rotate = gEngines.rotate,
+                   up = gEngines.up,
+                   fwd = gEngines.fwd,
+                   hold = gEngines.hold
+                 }) {
+    /*const nUp = alleviate(gEngines.up, up, gEngines.fwd, fwd);
+    const nFwd = alleviate(gEngines.fwd, fwd, gEngines.up, up);*/
+    const nUp = deadZone(gEngines.up);
+    const nFwd = deadZone(gEngines.fwd);
+    if ((nUp != gEngines.up) || (nFwd != gEngines.fwd)) {
+        gEngines.up = nUp;
+        gEngines.fwd = nFwd;
+        await roboarm.set(gEngines);
+        await wait(200);
+    }
+
+    gEngines.rotate = rotate;
+    gEngines.up = up;
+    gEngines.fwd = fwd;
+    gEngines.hold = hold;
+    await roboarm.set(gEngines);
+    await wait(300);
+}
+
+function initButtons(main) {
+    function update() {
+        horz.value = gEngines.rotate;
+        vert.value = gEngines.up;
+        fwd.value = gEngines.fwd;
+    }
+
+    async function set() {
+        gEngines.rotate = horz.value;
+        gEngines.up = vert.value;
+        gEngines.fwd = fwd.value;
+        await roboarm.set(gEngines);
+    }
+
+    const left = main.querySelector('.buttons .left');
+    const center = main.querySelector('.buttons .center');
+    const right = main.querySelector('.buttons .right');
+    const topVert = main.querySelector('.buttons .top.vert');
+    const bottom = main.querySelector('.buttons .bottom');
+    const forward = main.querySelector('.buttons .forward');
+    const backward = main.querySelector('.buttons .backward');
+    const sleep = main.querySelector('.buttons .sleep');
+    const topHorz = main.querySelector('.buttons .top.horz');
+
+    const horz = main.querySelector('.inputs .horz');
+    const vert = main.querySelector('.inputs .vert');
+    const fwd = main.querySelector('.inputs .fwd');
+
+    horz.onchange = set;
+    vert.onchange = set;
+    fwd.onchange = set;
+
+    update();
+
+    left.onclick = async () => {
+        await command({ rotate: 0 });
+        update();
+    };
+
+    right.onclick = async () => {
+        await command({ rotate: 180 });
+        update();
+    };
+
+    center.onclick = async () => {
+        await command({ rotate: 90 });
+        update();
+    };
+
+    topVert.onclick = async () => {
+        await command({ up: 70, fwd: 170 });
+        update();
+    };
+
+    bottom.onclick = async () => {
+        await command({ up: 180, fwd: 50 });
+        update();
+    };
+
+    forward.onclick = async () => {
+        await command({ up: 70, fwd: 70 });
+        update();
+    };
+
+    backward.onclick = async () => {
+        await command({ fwd: 180 });
+        update();
+    };
+
+    sleep.onclick = async () => {
+        await command({ up: 0, fwd: 158 });
+        update();
+    };
+
+    topHorz.onclick = async () => {
+        await command({ up: 0, fwd: 90 });
+        update();
+    };
+}
 
 
 function main() {
     const main = window.document.getElementById('Main');
-    initRotate(main);
-    initForwardBackWard(main);
+    //initRotate(main);
+    //initForwardBackWard(main);
     initKeep(main);
+    initButtons(main);
 }
 
 export default {
--- modules/roboarm.js
+++ modules/roboarm.js
@@ -1,70 +1,29 @@
-const endpoint = 'http://192.168.0.21';
-const eRotate = 4;
-const eUpDown = 1;
-const eForwardBackward = 2;
-const eKeep = 3;
-const COUNT_LOW = 1638;
-const COUNT_HIGH = 7864;
-const WIDTH = COUNT_HIGH - COUNT_LOW;
+const endpoint = 'http://192.168.0.27';
 
-async function rotate(value) {
+async function set(engines) {
     try {
-        await fetch(`${endpoint}?${eRotate}:${value}`);
+        await fetch(`${endpoint}/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `rotate=${engines.rotate}&up=${engines.up}&fwd=${engines.fwd}&hold=${engines.hold}`
+        });
     } catch (e) {}
-}
-
-async function upDown(value) {
-    try {
-        await fetch(`${endpoint}?${eUpDown}:${value}`);
-    } catch (e) {}
-}
-
-async function forwardBackward(value) {
-    try {
-        await fetch(`${endpoint}?${eForwardBackward}:${value}`);
-    } catch (e) {}
-}
-
-function getValueFromRange(current, max) {
-    const step = max / WIDTH;
-    return Math.trunc(current / step + COUNT_LOW);
-}
-
-function inverse(value) {
-    return Math.abs(value - COUNT_HIGH - COUNT_LOW);
-}
-
-function getBounds() {
-    return {
-        min: COUNT_LOW,
-        max: COUNT_HIGH
-    };
 }
 
 function getStartParameters() {
     return {
-        rotate: COUNT_LOW,
-        up: COUNT_LOW,
-        forward: COUNT_LOW,
-        hold: 2000
+        rotate: 90,
+        up: 30,
+        fwd: 30,
+        hold: 0
     }
 }
 
-async function set(engines) {
-    try {
-        await fetch(`${endpoint}?${eRotate}:${engines.rotate}&${eUpDown}:${engines.up}&${eForwardBackward}:${engines.forward}&${eKeep}:${engines.hold}`);
-    } catch (e) {}
-}
-
 export default {
-    rotate,
-    upDown,
-    forwardBackward,
-    getValueFromRange,
-    inverse,
-    getBounds,
-    getStartParameters,
     set,
-    hold: 2850,
-    release: 2000
+    getStartParameters,
+    hold: 30,
+    release: 0
 }
